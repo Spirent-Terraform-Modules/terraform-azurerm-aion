f83717e6049c36857e91ecd4509d418bf9f16886
radhika bhat <radhika.bhat@spirent.com>
2021-02-08 19:26:06 +0530

    Azure AION deployment code review (#1)
    * added new files for AION deployment
    
    * cleaned README file
    
    * fix spaces, obsolete coding style, virtual_network_name variable
    
    * removed unnecessary variable
    
    * cleaned example , README ,added  version file
    
    * added provider in version file
    
    * added provider block in version file
    
    * made version corrections

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
-# terraform-azurerm-aion
+# Spirent AION Platform Terraform
+
+![Image of Spirent AION](./images/aion.jpg)
+
+## Description
+
+Run Spirent AION platform instances.  After Terraform apply finishes you will be able to point your browser at the instance_public_ips addresses.
+
+If you would like to configure the Spirent AION platform in a web browser set the variable enable_provisioner=false.  When enable_provisioner=true the instance will be configured.  However, license entitlement & product installation will need to be completed in your web browser (see below).  Login to the platform instance https://<your_public_ip> using the values of admin_email and admin_password.
+
+### Add License Entitlements
+1. From "Settings" <img src="./images/aion_settings.jpg" width="22" height="22"/> navigate to "License Manager", "Entitlements"
+2. Click "Install Entitlements"
+3. Use one of the following methods to add entitlements (#1 is prefered)
+   1. Login to <your_org>.spirentaion.com and select entitlements to host in the new instance\
+      **Note:** Hosted entitlements should be released before destroying the instance.  When entitlements are not released you will need to contact Spirent support to release them for you.
+   2. Install a license entitlement file obtained from Spirent support
+
+### Add Products
+1. From "Settings" <img src="./images/aion_settings.jpg" width="22" height="22"/> navigate to "Settings", "Add New Products"
+2. Click "Install New Products"
+3. Select products and versions and click "Install"
+
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+## Requirements
+
+| Name | Version |
+|------|---------|
+| terraform | >= 0.13.0 |
+| azurerm | >=2.37.0 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| azurerm | >=2.37.0 |
+| null | n/a |
+| template | n/a |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| admin\_email | Cluster admin user email. Use this to login to instance web page.  Default is obtained from AION user information. | `string` | `""` | no |
+| admin\_first\_name | Cluster admin user first name. Default is obtained from AION user information. | `string` | `""` | no |
+| admin\_last\_name | Cluster admin user last name.  Default is obtained from AION user information. | `string` | `""` | no |
+| admin\_password | Cluster admin user password. Use this to login to to the instance web page. | `string` | n/a | yes |
+| admin\_username | Administrator user name. | `string` | n/a | yes |
+| aion\_image\_name | AION image created from private vhd file | `string` | n/a | yes |
+| aion\_password | AION user password for aion\_url | `string` | n/a | yes |
+| aion\_url | AION URL | `string` | n/a | yes |
+| aion\_user | AION user registered on aion\_url | `string` | n/a | yes |
+| cluster\_names | Instance cluster names.  List length must equal instance\_count. | `list(string)` | `[]` | no |
+| dest\_dir | Destination directory on the instance where provisioning files will be copied | `string` | `"~"` | no |
+| enable\_provisioner | Enable provisioning | `bool` | `true` | no |
+| http\_enabled | Allow HTTP access as well as HTTPS.  Normally this is not recommended. | `bool` | `false` | no |
+| ingress\_cidr\_blocks | List of management interface ingress IPv4/IPv6 CIDR ranges. | `list(string)` | <pre>[<br>  "0.0.0.0/0"<br>]</pre> | no |
+| instance\_count | Number of instances to create. | `number` | `1` | no |
+| instance\_name | Name assigned to the AION instance.  An instance number will be appended to the name. | `string` | `"aion"` | no |
+| instance\_size | The Azure Virtual Machine SKU. | `string` | n/a | yes |
+| local\_admin\_password | Cluster local admin password for instance SSH access.  Will use admin\_password if not specified. | `string` | `""` | no |
+| metrics\_opt\_out | Opt-out of Spirent metrics data collection | `bool` | `false` | no |
+| mgmt\_plane\_subnet\_id | Management public Azure subnet ID. | `string` | `""` | no |
+| node\_names | Instance cluster node names.  List length must equal instance\_count. | `list(string)` | `[]` | no |
+| node\_storage\_provider | Cluster node storage provider | `string` | `"local"` | no |
+| node\_storage\_remote\_uri | Cluster node storage URI.  Leave blank for default when provider is local | `string` | `""` | no |
+| private\_key\_file | File path to private key | `string` | n/a | yes |
+| public\_key | File path to public key. | `string` | n/a | yes |
+| resource\_group\_location | RG location in Azure | `string` | `"West US 2"` | no |
+| resource\_group\_name | RG name in Azure | `string` | `"default"` | no |
+| storage\_account\_name | Storage account in Azure. | `string` | `"radhikawestus2"` | no |
+| virtual\_network\_name | VNET name in Azure | `string` | `"STCv"` | no |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| instance\_ids | List of instance IDs |
+| instance\_private\_ips | List of private IP addresses assigned to the instances, if applicable |
+| instance\_public\_ips | List of public IP addresses assigned to the instances, if applicable |
+
+<!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
---
+++ main.tf
@@ -0,0 +1,175 @@
+resource "azurerm_network_security_group" "mgmt_plane" {
+
+  name                = "nsg-${var.instance_name}"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "ssh"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefixes    = var.ingress_cidr_blocks
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "http"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = 80
+    source_address_prefixes    = var.ingress_cidr_blocks
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "https"
+    priority                   = 103
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefixes    = var.ingress_cidr_blocks
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "application-usage"
+    priority                   = 104
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "64000"
+    destination_port_range     = "64999"
+    source_address_prefixes    = var.ingress_cidr_blocks
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "icmp"
+    priority                   = 106
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "icmp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefixes    = var.ingress_cidr_blocks
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_public_ip" "aion" {
+  count               = var.instance_count
+  name                = "publicip-${var.instance_name}-${count.index}"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "mgmt_plane" {
+  count               = var.instance_count
+  name                = "mgmt-nic-${var.instance_name}-${count.index}"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "ipc-mgmt-${var.instance_name}-${count.index}"
+    subnet_id                     = var.mgmt_plane_subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = element(azurerm_public_ip.aion.*.id, count.index)
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "mgmt_plane" {
+  count                     = var.instance_count
+  network_interface_id      = element(azurerm_network_interface.mgmt_plane.*.id, count.index)
+  network_security_group_id = azurerm_network_security_group.mgmt_plane.id
+}
+
+data "azurerm_image" "aion" {
+  name                      = var.aion_image_name
+  resource_group_name       = var.resource_group_name
+}
+
+data "template_file" "setup_aion" {
+  count                     = var.enable_provisioner ? var.instance_count : 0
+  template                  = file("${path.module}/setup-aion.tpl")
+  vars = {
+    script_file             = "${var.dest_dir}/setup-aion.py"
+    platform_addr           = azurerm_linux_virtual_machine.aion[count.index].public_ip_address
+    aion_url                = var.aion_url
+    aion_user               = var.aion_user
+    aion_password           = var.aion_password
+    cluster_name            = length(var.cluster_names) < 1 ? "" : var.cluster_names[count.index]
+    node_name               = length(var.node_names) < 1 ? "" : var.node_names[count.index]
+    admin_email             = var.admin_email
+    admin_first_name        = var.admin_first_name
+    admin_last_name         = var.admin_last_name
+    admin_password          = var.admin_password
+    local_admin_password    = var.local_admin_password
+    node_storage_provider   = var.node_storage_provider
+    node_storage_remote_uri = var.node_storage_remote_uri
+    metrics_opt_out         = var.metrics_opt_out
+    http_enabled            = var.http_enabled
+  }
+}
+
+# Create AION VM
+resource "azurerm_linux_virtual_machine" "aion" {
+  count                 = var.instance_count
+  name                  = "${var.instance_name}${count.index}"
+  location              = var.resource_group_location
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = [element(azurerm_network_interface.mgmt_plane.*.id, count.index)]
+  size                  = var.instance_size
+  admin_username        = var.admin_username
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.public_key)
+  }
+
+  os_disk {
+    name                 = "osdisk-${var.instance_name}-${count.index}"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_id        = data.azurerm_image.aion.id
+}
+
+# provision the AION VM
+resource "null_resource" "provisioner" {
+  count = var.enable_provisioner ? var.instance_count : 0
+  connection {
+    host        = azurerm_linux_virtual_machine.aion[count.index].public_ip_address
+    type        = "ssh"
+    user        = var.admin_username
+    private_key = file(var.private_key)
+  }
+
+  # copy install script
+  provisioner "file" {
+    source      = "${path.module}/setup-aion.py"
+    destination = "${var.dest_dir}/setup-aion.py"
+  }
+
+  provisioner "file" {
+    content     = data.template_file.setup_aion[count.index].rendered
+    destination = "${var.dest_dir}/setup-aion.sh"
+  }
+
+  # run setup AION
+  provisioner "remote-exec" {
+     inline = [
+       "bash ${var.dest_dir}/setup-aion.sh"
+     ]
+   }
+}
